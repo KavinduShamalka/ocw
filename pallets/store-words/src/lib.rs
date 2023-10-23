cba5ee04018d97fe2f95b53a16bb863e62e46ef1
KavinduShamalka <kavindusuraweera27@gmail.com>
2023-10-23 06:12:36 +0530

    change cargo.toml

--- pallets/store-words/src/lib.rs
+++ pallets/store-words/src/lib.rs
@@ -1,8 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
 
 #[frame_support::pallet]
@@ -80,6 +77,5 @@ pub mod pallet {
 			Ok(())
 
 		}
-
 	}
 }
