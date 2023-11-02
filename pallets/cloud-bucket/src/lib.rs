3cee3b1da8685b9ea3eaafeadf3647c24db3f96a
Kavindu <kavindu@MacBook-Prox.local>
2023-11-02 16:39:30 +0530

    initial commit

---
+++ pallets/cloud-bucket/src/lib.rs
@@ -0,0 +1,207 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_runtime::offchain::{http,Duration,};
+
+use frame_support::{dispatch::GetDispatchInfo, traits::UnfilteredDispatchable};
+
+use sp_core::crypto::KeyTypeId;
+
+extern crate cloud_mod_lib;
+// ...
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+
+// ...
+
+pub mod crypto {
+	use super::KEY_TYPE;
+	use sp_runtime::{
+		app_crypto::{app_crypto, sr25519}, MultiSignature, MultiSigner
+	};
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+
+		// implemented for runtime
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+}
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::{*, DispatchResult};
+	use frame_system::pallet_prelude::{*, OriginFor};
+	use frame_system::offchain::{ 
+		AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer,
+	};
+	use scale_info::prelude::string::String;
+	use frame_support::sp_io::offchain;
+	use codec::alloc::string::ToString;
+	use sp_std::vec::Vec;
+	// use sp_std::{collections::vec_deque::VecDeque, str};
+	use sp_std::str;
+	// const WORD_VEC_LEN: usize = 10;
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(_);
+
+	//Config
+	#[pallet::config]
+	pub trait Config: CreateSignedTransaction<Call<Self>> + frame_system::Config {
+
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+
+		type RuntimeCall: Parameter + UnfilteredDispatchable<RuntimeOrigin = Self::RuntimeOrigin> + GetDispatchInfo;
+	}
+
+	//Word Struct
+	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo)]
+	pub struct Word {
+		pub word: String
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn info)]
+	pub type WordSave<T> = StorageValue<_, Word>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn store)]
+	pub type WordStore<T> = StorageValue<_, Word>;
+
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		WordStored { word: T::AccountId },
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+	
+		NoneValue,
+		/// Errors should have helpful documentation associated with them.
+		StorageOverflow,
+
+		/// Error returned when fetching github info
+		HttpFetchingError,
+
+		UnknownOffchainMux,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+
+		fn offchain_worker(block_number: BlockNumberFor<T>) {
+			
+			log::info!("Hello from ⛓️‍💥 offchain worker ⛓️‍💥.");
+			log::info!("🌐⛓️ Current block: {:?} 🌐⛓️", block_number.clone());
+
+			match Self::fetch_word_and_send_signed() {
+				Ok(result) => log::info!("Word: {}", result),
+				Err(error) => log::info!("Error fetching word: {}", error),
+			}
+		}
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+
+		//Store word
+		#[pallet::call_index(0)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn save_word(origin: OriginFor<T>, word: String) -> DispatchResult {
+
+			let sender = ensure_signed(origin)?;
+
+			let new_word = Word {
+				word,
+			};
+
+			<WordSave<T>>::put(new_word);
+
+			Self::deposit_event(Event::WordStored { word: sender });
+
+			log::info!("Hello from word Save.");
+
+			Ok(())
+
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+
+		//Fetch word from the api
+		fn fetch_word() -> Result<String, http::Error> {
+
+			//set deadline
+			let deadline = offchain::timestamp().add(Duration::from_millis(2_000));
+
+			//set get request
+			let request = http::Request::get("https://random-word-api.herokuapp.com/word");
+
+			let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
+
+			let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+			
+			//check response is successfull
+			if response.code != 200 {
+				log::warn!("Unexpected status code: {}", response.code);
+				return Err(http::Error::Unknown)
+			}
+			
+			let body = response.body().collect::<Vec<u8>>();
+
+			let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
+					log::warn!("No UTF8 body");
+					http::Error::Unknown
+			})?;
+
+			let result = body_str.to_string();
+
+			Ok(result)
+		}
+
+
+		fn fetch_word_and_send_signed() -> Result<String, &'static str> {
+
+			let signer = Signer::<T, T::AuthorityId>::all_accounts();
+
+			if !signer.can_sign() {
+				return Err(
+					"No local accounts available. Consider adding one via `author_insertKey` RPC.",
+				)
+			}
+
+			let word = Self::fetch_word().map_err(|_| "Failed to fetch word")?;
+
+			let results = signer.send_signed_transaction(|_account| {
+
+				Call::save_word { word: word.clone() }
+			});
+
+			for (acc, res) in &results {
+				match res {
+					Ok(()) => log::info!("[{:?}] Submitted word of {}", acc.id, word.clone()),
+					Err(e) => log::error!("[{:?}] Failed to submit transaction: {:?}", acc.id, e),
+				}
+			}
+
+			Ok(word)
+		}
+
+	}
+}
