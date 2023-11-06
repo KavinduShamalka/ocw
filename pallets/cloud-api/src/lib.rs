62e136599d96a857c0391a1e81ff3fe9d7b700e9
Gimhani <Gimhani@Gimhani-MacBook-Pro.local>
2023-11-06 13:48:32 +0530

    add file upload and delete features

--- pallets/cloud-api/src/lib.rs
+++ pallets/cloud-api/src/lib.rs
@@ -3,6 +3,8 @@
 use sp_runtime::offchain::{http, Duration};
 // use sp_runtime::offchain::http::Error;
 
+// use frame_support::{dispatch::GetDispatchInfo, traits::UnfilteredDispatchable};
+
 pub use pallet::*;
 
 use sp_core::crypto::KeyTypeId;
@@ -53,6 +55,8 @@ pub mod pallet {
 	};
 	use scale_info::prelude::string::String;
 	use sp_std::{collections::vec_deque::VecDeque, str, vec};
+	use sp_std::vec::Vec;
+	use sp_runtime::offchain::http::Method;
 	// const WORD_VEC_LEN: usize = 10;
 
 	#[pallet::pallet]
@@ -66,15 +70,22 @@ pub mod pallet {
 		type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
 	}
 
-	//Word Struct
+	//Bucket name Struct
 	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo)]
 	pub struct BucketName {
 		pub name: String,
 	}
 
+	//Folder name structure
 	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo)]
 	pub struct FolderName {
 		pub fname: String,
+	}
+
+	//File name structure
+	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo, Debug)]
+	pub struct File {
+		pub file: Vec<u8>,
 	}
 
 	#[pallet::storage]
@@ -89,11 +100,17 @@ pub mod pallet {
 	#[pallet::getter(fn store)]
 	pub type BucketNameStore<T> = StorageValue<_, VecDeque<String>, ValueQuery>;
 
+	#[pallet::storage]
+	#[pallet::getter(fn file)]
+	pub type FileSave<T> = StorageValue<_, Vec<u8>>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		BucketCreated { name: T::AccountId },
-		FolderCreated { fname: T::AccountId }
+		FolderCreated { fname: T::AccountId },
+		FileFetched { file: T::AccountId },
+		FileDeleted { file: T::AccountId },
 	}
 
 	// Errors inform users that something went wrong.
@@ -116,15 +133,15 @@ pub mod pallet {
 			log::info!("Hello from ⛓️‍💥 cloud offchain worker ⛓️‍💥.");
 			log::info!("🌐⛓️ Current block: {:?} 🌐⛓️", block_number);
 
-			match Self::create_bucket_request() {
-				Ok(word) => log::info!("Bucket: {:?} created successfully", word),
-				Err(_) => log::info!("Error creating bucket"),
-			}
+			// match Self::create_bucket_request() {
+			// 	Ok(word) => log::info!("Bucket: {:?} created successfully", word),
+			// 	Err(_) => log::info!("Error creating bucket"),
+			// }
 
-			match Self::create_folder_in_bucket() {
-				Ok(word) => log::info!("Folder: {:?} created successfully", word),
-				Err(_) => log::info!("Error creating folder"),
-			}
+			// match Self::create_folder_in_bucket() {
+			// 	Ok(word) => log::info!("Folder: {:?} created successfully", word),
+			// 	Err(_) => log::info!("Error creating folder"),
+			// }
 
 			// match Self::create_bucket_request() {
             //     Ok(bucket_name) => {
@@ -133,6 +150,22 @@ pub mod pallet {
             //     }
             //     Err(_) => log::info!("Error creating bucket"),
             // }
+
+			// match Self::_file_upload() {
+			// 	Ok(_) => log::info!(" 📁 ✅ 〰️〰️ File uploaded 〰️〰️ 📁 ✅"),
+			// 	Err(error) => log::info!("📁 ➡️ ⭕⭕ ❌❌❌❌ Error file uploading ===> : {:#?} ⭕⭕ ❌❌❌❌", error)
+			// }
+
+			// match Self::file_delete() {
+			// 	Ok(_) => log::info!("File deleted"),
+			// 	Err(error) => log::info!("Error file deleting ===> : {:#?}", error)
+			// }
+
+			
+			match Self::file_delete() {
+				Ok(code) => log::info!("✅✅✅✅ File deleted : {} ✅✅✅✅", code),
+				Err(error) => log::info!("❌❌❌❌ Error file deleting ===> : {:#?} ❌❌❌❌", error)
+			}
 		}
 	}
 
@@ -170,11 +203,51 @@ pub mod pallet {
 
 			Ok(())
 		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn upload_file(origin: OriginFor<T>, _file: Vec<u8>) -> DispatchResult {
+			
+			log::info!("Hello from upload file");
+			
+			let sender = ensure_signed(origin)?;
+
+			// let new_file = FileName { filename };
+
+			// <FileNameSave<T>>::put(new_file);
+			
+			<FileSave<T>>::put(_file);
+
+
+			Self::deposit_event(Event::FileFetched { file: sender });
+
+		
+			Ok(())
+		}
+
+		#[pallet::call_index(3)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn delete_file(origin: OriginFor<T>) -> DispatchResult {
+			
+			log::info!("Hello from delete file");
+			
+			let sender = ensure_signed(origin)?;
+
+			// let new_file = FileName { filename };
+
+			// <FileNameSave<T>>::put(new_file);
+
+
+			Self::deposit_event(Event::FileDeleted { file: sender });
+
+		
+			Ok(())
+		}
 	}
 
 	impl<T: Config> Pallet<T> {
 		//fetch data from the url
-		fn create_bucket_request() -> Result<String, http::Error> {
+		fn _create_bucket_request() -> Result<String, http::Error> {
 
 			let url = "https://storage.googleapis.com/storage/v1/b?project=intern-storage-apis";
 			
@@ -209,7 +282,7 @@ pub mod pallet {
     		Ok("fcx-text-bucket1".to_string())
 		}
 
-		fn create_folder_in_bucket() -> Result<String, http::Error> {
+		fn _create_folder_in_bucket() -> Result<String, http::Error> {
 		
 
 			let json_data = r#"{"name": "text-folder-newest"}"#;
@@ -247,6 +320,98 @@ pub mod pallet {
 			Ok("text-folder-newest".to_string())
 		}
 		
+		fn _file_upload() -> Result<(), http::Error> {
+			//set deadline
+			let deadline = offchain::timestamp().add(Duration::from_millis(2_000));
+
+			let file_fetch = FileSave::<T>::try_get();
+
+			let value = match file_fetch {
+				Ok(res) => res,
+				Err(_) => Vec::new(), //retuen an empty vec
+			};
+
+			//send post request to file upload
+			let request = http::Request::post("https://storage.googleapis.com/upload/storage/v1/b/fcx-text-bucket1/o?uploadType=media&name=pic.jpeg", vec![value.clone()])
+			.add_header("Authorization", "Bearer ya29.c.c0AY_VpZjH50kvwF1pDwMtucqZZqvbfd9BmcD8tnjk7QO7fhAxzLTKwBHtq_2pxMdWqXWpvOuPFrXDEpw1osAlMOZpAL_RBWOdlj3XJeonXrjgjB-yupc4N5DGwx_-0Ip8yuCEUjOu1xsV4ezcfCwPc03oKDfbcFYO1rStdjwXyyUmnUplL-YriIYQJIfU1nYV33K0EXWTCTmxb8raRNMOUNABOx0wk5nzsyRXJ7cIUO6rL557JhtLv2oEdMG_Jf-OPGtFHlNjTsMdWN7vRmEUxKuZ7c_Kn3cIGI6veV6M3ke7dLUhxe7BSwHqIXPxNaMoeCRHDfASB52mgIiDNpEE-b7JIwlIyYKhFXy32tCobZN1jhH6UsPRV8JDwc_nZ7uxTdoJDgL399DarQ1Wd9-yyUlt7xiuvWp4U5p3lJj8Ws0fsqXXI7aeu01Qlhz5cY7f2yJs_bp5oMwlk_0r01MgMVa4hI_-W5nB-hr8eOrw6MMq2wjerUslrIuOzkzsR8ln6zxlIxJ_yYOvuXRkBilR2nsZYIMzXWFph5k3Z7hzYa6whis27f7Y18txiYJ89tgaqS8iXOnOB_pyyecbFUdFpzIMySrId6c2-jx8XkO2tyfpOzM7yMz0WenIlfleRsXZo8v2MsqRdrxIYsn7rb-owuVB2RJ4luOQc0_vXcYXrUjx1hhbO6q_Ongz8vBa23itahiypzMF_byoIYmld80npR466tRVRyhlpUSvc56bV248y7bFQROWoZgMFMQVUM5pkFOB0Rx2f20Jv4nJcazQIjqhW8hyFamu6MMBdZebV1z2wWekhyXkidxY9sa01zhspbc-R2vXWBooimn4_WcjSOiVqX__zrveiJ3dMoXgU31Ofeagzw2UluakZ-BjbwzydQgaSrJ-O4U2aRI1e3y5xJJ-_3_nR5skslakQXz9zouqrsjQgccI12OfZwSeZi2boeXx9oxstYIzOOjf6vkp1y3rOXlUnj2y8SqIQxsImO7zdf_agMQgfU8")
+			.add_header("Content-Type", "image/jpeg");
+
+			let pending = request
+			.deadline(deadline)
+			.body(vec![value.clone()])
+			.send()
+			.map_err(|_| http::Error::IoError)?;
+
+			// Wait for response 
+			let response = pending
+			.try_wait(deadline)
+			.map_err(|_| http::Error::DeadlineReached)??;
+
+			log::info!("Status code: {:#?}", response.code);
+
+			//check response is successfull
+			if response.code != 200 {
+				log::warn!("🔴 🔴 🔴 🔴 Unexpected status code when uploading file: {} 🔴 🔴 🔴 🔴", response.code);
+				return Err(http::Error::Unknown)
+			}
+			
+			Ok(())
+
+
+		}
+
+		fn file_delete() -> Result<u16, http::Error> {
+			//set deadline
+			let deadline = offchain::timestamp().add(Duration::from_millis(2_000));
+
+			// let file_fetch = FileSave::<T>::try_get();
+
+			// let value = match file_fetch {
+			// 	Ok(res) => res,
+			// 	Err(_) => Vec::new(), //retuen an empty vec
+			// };
+
+			let json_body = r#"{}"#;
+
+			//send post request to file upload
+			let delete_requsest = http::Request::default().method(Method::Delete)
+            .url("https://storage.googleapis.com/storage/v1/b/fcx-text-bucket1/o/cat.jpeg?key=AIzaSyA6f3dyDiLIkizS6PB3UZY-a1CcDksVEAc")
+            .add_header("Authorization", "Bearer ya29.c.c0AY_VpZgXFFw88RhSsBxDP7A0I73sXVGeCMouArPdH6UDIHs2alJm9vs0hWOent_5XamcvpNTB1YJ8GWvWcHrfez6MumFMDbNrTvZk2P22EHNpv_Ino7FGwd377-fMweT5jqsdwEDaM7BdidmR8bU1vLqbMZy29PPLRRsVKSRCgvQ0fU6T0e4A6nfp1h7hhCs6foyPiXaRpC4xwcgI713Ayu0np3PgI53FxOUWcbor9vUABkP4cWdLdNBINKmeAA2h9VEYkkMh1oVXD4t1M6dJ7WP394IhahXy8pRBOO8609ZxRKeh48Ew4SXANMqRYWMjITe5OxBlAC8SjJEv7GrMGZLhchfoXppIeEJS9FHX5MOJqKJb9pMMcXWhnb9O95KACWNN397Pr53jJ-qVz8g-s0ikUVu-5msMYsMmZf4O91IyjalrmwX2zV2a550vfkUZmxXpOFi5ISFgQj00cWrQuWqtOb4sb7m_RsZan8O3Qweax6QpSsl9nbrfSbVs4p5W-tJ5aF4y4fWiW2_1mmScoqv224_yXBkuQVfY8s1UBs_5FjjIbduXMjueshuiiZS5Xkek9OuM0-vqOilqO3rlum_hsfursnfcycQvegexaf80JRvma32c_dQJSO23lsIu348W6dx0o0pybbvz7B5bQ6VWchM9fgghc90I9BWV2xSvMdXur16dd_8uh9xSotlo8qi888O0Vtahq5M1qY7XqbfscSuJgV7dlB0RwX72oWt9tgMFnpzntxtWrVRRXdqVWapYik8c1jm21-uptwBmZnQUmS4W1ZVqhkWBW6RXyMylQ3BwWvZ6o7Qlv-ilXuqkcg6qgpnm85FUVro_wvr-Foq8OyjUcyjnk4_a_Fsh4ydR3olbgtsruMrSlV5wmkUY1SaF3rs6ffz2cb1QI16yn_aib_vF8q19ekg2M5eRS6wjuMfhROy_eOctfj9rWuOka7b2nzxOUvUyRFlZUMFQSkmciVuaaY7-dBs4J8Vd6dZp9tlafpyqem")
+            .add_header("Accept", "image/jpeg");
+
+
+			let pending = delete_requsest
+			.deadline(deadline)
+			.body(vec![json_body])
+			.send()
+			.map_err(|_| http::Error::IoError)?;
+
+			// Wait for response 
+			let response = pending
+			.try_wait(deadline)
+			.map_err(|_| http::Error::DeadlineReached)??;
+
+			log::info!("Status code: {:#?}", response.code);
+
+			//check response is successfull
+			if response.code != 204 {
+				log::warn!("❌ ❌ ❌ ❌Unexpected status code when deleting file: {} ❌ ❌ ❌ ❌", response.code);
+				return Err(http::Error::Unknown)
+			}
+
+			//call signed transaction
+			let signer = Signer::<T, T::AuthorityId>::all_accounts();
+
+			signer.send_signed_transaction(|_account| {
+				Call::delete_file { }
+			});
+
+			
+			Ok(response.code)
+
+
+		}
+
 
 
 		/// A helper function to fetch the word and send signed transaction.
