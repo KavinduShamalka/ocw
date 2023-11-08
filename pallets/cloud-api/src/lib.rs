0cfe8f2d9afa882577341464f834ab8a58a2cf76
Gimhani <Gimhani@192.168.1.3>
2023-11-08 12:08:46 +0530

    not complete

--- pallets/cloud-api/src/lib.rs
+++ pallets/cloud-api/src/lib.rs
@@ -73,7 +73,7 @@ pub mod pallet {
 
 	//Bucket name Struct
 	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo, Debug)]
-	pub struct BucketName {
+	pub struct BucketS {
 		pub name: String,
 	}
 
@@ -91,6 +91,19 @@ pub mod pallet {
 		pub bucket_name: String,
 	}
 
+	//Folder structure
+	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo, Debug)]
+	pub struct Folder {
+		pub folder_name: String,
+		pub bucket_name: String,
+	}
+
+	//File structure
+	#[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo, Debug)]
+	pub struct FileDown {
+		pub file_name: String,
+		pub bucket_name: String,
+	}
 	// //File name structure
 	// #[derive(Encode, Decode, Clone, PartialEq, Default, TypeInfo, Debug)]
 	// pub struct FileName {
@@ -104,7 +117,7 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn fstore)]
-	pub type FolderNameSave<T> = StorageValue<_, FolderName>;
+	pub type FolderCreate<T> = StorageValue<_, Folder>;
 
 	// #[pallet::storage]
 	// #[pallet::getter(fn store)]
@@ -133,6 +146,10 @@ pub mod pallet {
 	#[pallet::getter(fn fnamestore)]
 	pub type FileDelete<T> = StorageValue<_, String>;
 
+	#[pallet::storage]
+	#[pallet::getter(fn fdown)]
+	pub type FileDownload<T> = StorageValue<_, FileDown>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -141,6 +158,7 @@ pub mod pallet {
 		FileFetched { file: T::AccountId },
 		FileDeleted { file: T::AccountId },
 		BucketDeleted { name: T::AccountId },
+		FileDownloaded { file: T::AccountId },
 	}
 
 	// Errors inform users that something went wrong.
@@ -169,8 +187,8 @@ pub mod pallet {
 			}
 
 			// match Self::create_folder_in_bucket() {
-			// 	Ok(word) => log::info!("Folder: {:?} created successfully", word),
-			// 	Err(_) => log::info!("Error creating folder"),
+			// 	Ok(word) => log::info!("📁✅ Folder: {:?} created successfully📁✅", word),
+			// 	Err(_) => log::info!("📁❌ Error creating folder 📁❌"),
 			// }
 
 			match Self::file_upload() {
@@ -187,6 +205,12 @@ pub mod pallet {
 				Ok(code) => log::info!("✅✅✅✅  Bucket deleted : {} ✅✅✅✅", code),
 				Err(error) => log::info!("❌❌❌❌ Error bucket deleting ===> : {:#?} ❌❌❌❌", error)
 			}
+
+			// match Self::file_download() {
+			// 	Ok(_) => log::info!(" 📁 ✅ 〰️〰️ File download 〰️〰️ 📁 ✅"),
+			// 	Err(error) => log::info!("📁 ➡️ ⭕⭕ ❌❌❌❌ Error file downloading ===> : {:#?} ⭕⭕ ❌❌❌❌", error)
+			// }
+
 		}
 	}
 
@@ -211,12 +235,12 @@ pub mod pallet {
 
 		#[pallet::call_index(1)]
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		pub fn save_folder_name(origin: OriginFor<T>, fname: String) -> DispatchResult {
+		pub fn create_folder(origin: OriginFor<T>,folder_name: String, bucket_name: String) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
-			let new_name = FolderName { fname };
+			let new_name = Folder { folder_name, bucket_name };
 
-			<FolderNameSave<T>>::put(new_name);
+			<FolderCreate<T>>::put(new_name);
 
 			Self::deposit_event(Event::FolderCreated { fname: sender });
 
@@ -280,6 +304,25 @@ pub mod pallet {
 			Ok(())
 		}
 
+		#[pallet::call_index(5)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn download_file(origin: OriginFor<T>,file_name: String, bucket_name: String) -> DispatchResult {
+			
+			log::info!("Hello from download file");
+			
+			let sender = ensure_signed(origin)?;
+
+			let new_file = FileDown { file_name, bucket_name };
+
+			<FileDownload<T>>::put(new_file);
+			
+			// <FileStore<T>>::insert(sender.clone(), filename, _file);
+
+			Self::deposit_event(Event::FileDownloaded { file: sender });
+
+		
+			Ok(())
+		}
 
 	}
 
@@ -337,22 +380,42 @@ pub mod pallet {
 		}
 
 		fn _create_folder_in_bucket() -> Result<String, http::Error> {
-		
 
-			let json_data = r#"{"name": "text-folder-newest"}"#;
+			let folder_create = FolderCreate::<T>::try_get();
+
+			let value = match folder_create {
+				Ok(res) => res,
+				Err(_) => Folder{folder_name: String::new(), bucket_name: String::new()}, 
+			};
+
+			let bucket_name = value.bucket_name;
+
+			let folder_name = value.folder_name;
+
+			// let json_data = r#"{"name": "text-folder-newest"}"#;
+
+			//let json_data = format!(r#"{{"label": "{}"}}"#, folder_name);
+
+			let json_data = format!(r#"{{"name": "{}", "folder": "{}"}}"#, folder_name, true);
+
 			
-			let url = "https://storage.googleapis.com/storage/v1/b/fcx-text-bucket1/managedFolders";
+			let url = format!("https://www.googleapis.com/storage/v1/b/{}/o", bucket_name);
 			
 		
-			let request = http::Request::post(url, vec![json_data])
-				.add_header("Authorization", "Bearer ya29.c.c0AY_VpZgaiaxV_kIuHiFgyhIJT9RYVvaYKcZM3zFIPe5LVrPLg7G0_m_U89a16j6WKAykpt7OIqcthKvzcrw0p1S7Rpz2uTUpMJSnQ7dTf0PAbQWH3z4Hsvqzt735wup4OeWJvx8m17yJALOrqzh6V56KhY0ffgpbUbhB2fgYe_J4jFO5MLhbGCA-A_0oa8Gw3EtpBMWEmbrDGuyAiDzTdsHVYLBp30I3ZZdUsC7NV-KRs9HLjSUHrzoyvzPMVmVKPPO0j06vDvF2TQGqZUAzsPyH3RruOOHh8F1xgs8jiI7qGV7Op6qISlYhDXdqyPtzpcCCTEMtbODDa8P9wW3yttZQLqpn1YKIC8eTx1Ep7weXn3BN8hXXpHDy92tnguMRbGBfL397KUyZza3Rpn2Bk4_7jcvl_J17ew56y-fvkl9Qv3QWatyBrBIIBV820_4eFfW3X3V-p0b6OWlrpfnQgt_xSzp5c2sxb2YzShucefYZqsOwrY-fQ-oVhIWjsU_6Zu8_VZfkeQ0Szktu7zzV_bIYp1cV4hfcOamoojbkj5F90V7bY3xu4bc8wQXiM-Rok3w7OZytb6Y5dbh-Vl8W5-5rt8y41Fs_MehzznMnBfabWrrYZ4yyjxu_8kjOwYJfdVRM_o5FatO89x0ju7fdIg7OghR0FvrY41leuV9rbm9Zjf0fq7n8iZ427kQZWXWQJMzgsWYbBRM5287SuFt5r7wWZfxody5lrilopjlfV8dzkgtsafo48eo9SokBp9BOQzwV-8Qazsk6pyj4wygM99V0q7v53ge3q8x2qwlbgxr1uM6kW83m2WIW0iQXxozc9QwtMzzmxh4Mb_tgZthFt2nZZ7dqxZwkwjs8o_92atbyZtUerrQd1oe3lnVg2-caXM33y0iZ-Fj-wIJvrWe3jX4_uJ6061xobYR6uOQtQOt67M8ebr42eU_taum-ylQ2XYBSxJVI6W4gopze0Xid53IojWfgeupleoozZ4pQwrlgfbFct-squth")
-				.add_header("Content-Type", "application/json");
+			// let request = http::Request::patch(url.as_str(), vec![json_data.clone()])
+			// 	.add_header("Authorization", "Bearer ya29.c.c0AY_VpZgoWbKGLPmCzClXU0SWItAji_pxxTAJxiqbXcmxQj70N6n0tGMflsFIhj3J-PSX5bj3JD_yAWymt4b0ymtCDXrvB26TJYwjpd7JWyLYOE3NM4lGxjzswfoeDoDxVpkM6sZi23ZSE_0w8VwQ8ptAodFDv9f9lrBK6if7v3NfZzcgDw480WQIDQtnEcLxAUA52XdWUENcCleym7B6ZS1b9Yd7SvqFIPE0J53ESdMhNEwKctSOlfrcQc9L2JEQCTXZmlCOCUs2TJt2lTcefgmG4ajNCAy-Vxk20N-J9SLIvq-xaWoJsNOWQU_RBheucN6LcEXCEp-jn_7O2QENSKZglW9BE1axgdsrPQVEmRPrtnRxA6H-ys5liVyNVIZFVee-7gIH400AkqsIngOh_Zs7JunbnFd7jOdxi1c5pzm5SB8d9kY6Sn37OpZ4msUMZMMJrshIlupe2rym8haxu4aQbWc1syZdtXJFYrta-Xwbo1VihiVu6Fxy8pQbaR-M6rlr9kMiaR8B2shd9pgpR-rcJdzRJiI-U5mXviupJ3eoRzZwdOWkJecuzOBxap36Zyl4Fefn2403YJQs2d5z5yli-sJg1_87khkh1ghzw-m6ra_Ur-M8VMcZMxYSgOgl70Bukav1njFF_-yV2j8sOYOcnp9YZX1tgYgZfVbBjq3ZFMoc0XqmoX0Mdw9rq25OnSbZMwZUj-nOrvhxypM30YeJ4psQbJ-yRgb9fM6nvdZ_vMvJF-3tRJm0Mart6OmsIJlI3BpcBsV8wxrMuQhXrFe-9atn-Mf05w4ivtoQvQRRrZog8oFonZ83i8YeIqJV5m3fR-6iwI3XVQQJjljj9c2slUx93_ik-cl53W6lqhqVYsinvrtfJFwjiBlc29QUR6uoy2ky5u7W0-kWZIiYk-YnWuJ6Q33qvIaJusov5-SpjnIw2V7BX6n-8BWR9Qs15jXM1f4izuRzfwWeRnSQhoZa1Sxl4hdtMUzwnvmlaq4lm2ZJnRrif3X")
+			// 	.add_header("Content-Type", "application/json");
+
+
+			let request = http::Request::post(url.as_str(), vec![json_data.clone()])
+            .add_header("Authorization", "Bearer ya29.c.c0AY_VpZjH01bqq9e5OS39VeB0-mckGh4A_rfUIAuDnJu7HEDOMNhJFR4CEya2uNMU475itsAHx8fZS9aZ1CXk2QbRakb3z8dN7nvY9RoFvKz5-nADXrOaFpEk69yVORbP644DKK9s5tacMUgdbCkzsmqLz-8qRFYpbSyJU2dmmcgffDKhchDUH8Nt3vTKv7rPZnC-2Zrm5EPJSJ2GsIKJb_C8GpmZ1oCAhlpdrx345FDGUicgL0r_xxtQBgJFURcougDaA-mxlAGGkEpAm9PG_N20rEq-0k6RoJxJGr0iCgUEZkjrJUOz6PC3ySSMckmTbJbcRueYOYR8ky1RevVcCrzm2gMJHJ6wahMvRFqy1FMVmeYiQG08MPvwq3hrXA8QXPjtT397P09lI2IxgkRBu5Vz0n_6hWjoSn0-oSk0Vd5R20BqibQtqV46eqxzzqla8_XMYySx5Oy8QQdJr7xIZznXZFuVQZh0_cl4j3FwFud1R3BxyhOzvpx8yRo8hjiW_MdoIid53y4wv_FQSzytZpwaQ8gpZFB1h531zhkvU4liajXsha1Vbu8qestuzeJdVsRtZpnkk_ql3RZYM8hZ8rtRQzxF07czjd2gvi_9IUg1qcuUYkMjweXdRzRm3nZYglZsIZVxyeaM_YeO5QR7ex9rRbud82hslecUXgXe0okzpkUicrjM5r7FqJZ1li3OymklkupRc1JnRXx2o2IoqblqrugJtYXJdISuYJSxQoFakBe0r3nJX88q7BJ7cOn4Qkelw46d9p03lVRQ4m59F4Z3h0u9dBXhxX51h7nRalhjWj60zx1wirRi_rI7iMddSjjB3j8tx9nmRXkS7bg8tcOkap2kSaZxbUV98YFpdbbzgnRR3qylZS5fmgm4zSqMXike1ta4wVe719zbblR3Jik3Zxi4VbbqIyp74fUajF899uyq_YnrBFSB_3jUuI_pdnkr13O-mBwWpbqqajg2MRFeJ73S8Iae7UY0IVcuU32elfSshWi1kF8")
+			.add_header("Content-Type", "application/json");
 		
 			let deadline = offchain::timestamp().add(Duration::from_millis(2_000));
 		
 			let pending = request
 				.deadline(deadline)
-				.body(vec![json_data])
+				.body(vec![json_data])  
 				.send()
 				.map_err(|_| http::Error::IoError)?;
 		
@@ -364,14 +427,15 @@ pub mod pallet {
 			// 	Err(err) => log::warn!("Error status code: {:?}", err)
 			// };
 
+			log::info!(" 📁 Status code: {:#?}", response.code);
+
 			if response.code != 200 {
-				log::warn!("Unexpected status code: {}", response.code);
+				log::warn!("📁🔴 Unexpected status code: {} 📁🔴", response.code);
 				return Err(http::Error::Unknown);
 			}
-
-			Self::fetch_folder_name_and_send_signed("text-folder-newest".to_string());
+			
 		
-			Ok("text-folder-newest".to_string())
+			Ok(folder_name)
 		}
 
 		fn bucket_delete() -> Result<u16, http::Error> {
@@ -471,8 +535,54 @@ pub mod pallet {
 			
 			Ok(())
 
-
 		}
+
+		// fn file_download() -> Result<(), http::Error> {
+		// 	//set deadline
+		// 	let deadline = offchain::timestamp().add(Duration::from_millis(2_000));
+
+		// 	let file_down = FileDownload::<T>::try_get();
+
+		// 	let value = match file_down {
+		// 		Ok(res) => res,
+		// 		Err(_) => FileDown{file_name: "".to_string(), bucket_name: "".to_string() }, 
+		// 	};
+
+		// 	let file_name = value.file_name;
+		// 	let bucket_name = value.bucket_name;
+
+		// 	let url = format!("https://storage.googleapis.com/storage/v1/b/{}/o/{}?alt=media", bucket_name, file_name);
+
+		// 	//send post request to file upload
+		// 	let request = http::Request::get(url.as_str())
+		// 	.add_header("Authorization", "Bearer ya29.c.c0AY_VpZg8bc3C9wddUNwsEobdH4juj-xwHLKQ64jCQOx5HtEs9f7958cotkhYcDely3pTFTCSgrEuKWLPg864In0HPtdvW4ATegVv9M_TaXm9RiAGhtvXdbi11oTn5cpu-bv2pw7yvi-CUdIpkkDfnNgPSTJRUONEVCfQp1PTYfhVytvB1S_qRV5Gg_sBPqPRvPpgJwO4YhUwPDi86uZ4vbGZhDcEHeAgaw-d3kl0bxVLiP98JAasHy7s9LhYbhOg4vOWTvvHhFVuNQbo1d28LGXFNrSossr6mrOfUEy8KR_rt_2Rl9REgan48uyivXh8l5ZFE9kN96xbAtAV_iI8vxzN2xbi9D1jH5k3ZwNKMfw53vEhvXrkrC-8V8kaE_OVAAV0YY0E400Aw1y186S8yh-zZyjJ8gwgf0_89W0kRZz8--oUUigl1OSBaIwV5xe-p0iinft8QaYd__gu48vaFX_3X1q_es7aMaq12pUMBRrS9dnZlqdXl7respzvoBs67dmWd_Mn1809efI9tUmR0jSXax8ZiV_cfY60hn5hmMX1I5IVM4sMdsqWW816VJ8rlRliRnxIedbRB0rU-ewQRbjVkmIsMtM3j9tfvSuUsUov6lr6Qqjq7mU15Z8Je1mg46J8YzYzZ1_1zwqb0eIOlx7etM7IsvUjmydmS_ZJe_BBFZcx-mWfW3j0R4MzW4B00OR0Wjvaa3Os36B5VIsMYsbe56VZkFtVqu5jn9FnSgRM7rOYbx-hO36sBjXiWJ1UBS9XB95QehvmlmSYe8UyyJolrm3gvWB3_Vsi4S6q2l9Q6jwxhuMsnuxb-w32rj3Be88e9ZUQkog6eSpXzt8JZdg628cJZ3jkk1R15ItmhrOf2thXtmei5s2WgzsdnvIYc4UMRqpvylvb_5amFglpsQe8-ZpqQuvm-xiQIgh5Ua6qd_s3OkO5ZzOuxs4vbkoVI8uZkkgRVywZb3by7BQf5IwpBmqp37I91b0k2Yh96odz_e0s20Isbog");
+
+		// 	let pending = request
+		// 	.deadline(deadline)
+		// 	.send()
+		// 	.map_err(|_| http::Error::IoError)?;
+
+		// 	// Wait for response 
+		// 	let response = pending
+		// 	.try_wait(deadline)
+		// 	.map_err(|_| http::Error::DeadlineReached)??;
+
+		// 	log::info!("🖌️Status code: {:#?}", response.code);
+
+		// 	//check response is successfull
+		// 	if response.code != 200 {
+		// 		log::warn!("🔴 🔴 🔴 🔴 Unexpected status code when downloading file: {} 🔴 🔴 🔴 🔴", response.code);
+		// 		return Err(http::Error::Unknown)
+		// 	}
+
+		// 	// Save the response body to a file
+		// 	let mut file = fs::File::create("/Users/Gimhani/Downloads").map_err(|_| http::Error::IoError)?;
+		// 	io::copy(&mut response, &mut file).map_err(|_| http::Error::IoError)?;
+		
+			
+		// 	Ok(())
+
+		// }
 
 		fn file_delete() -> Result<u16, http::Error> {
 			//set deadline
@@ -488,7 +598,6 @@ pub mod pallet {
 			let api_key = "AIzaSyBmwFlnn_aqaPBSkMntWe40qEn4yrY9BIQ";
 
 			let url = format!("https://storage.googleapis.com/storage/v1/b/fcx-text-bucket1/o/{}?key={}", value.clone(), api_key);
-
 
 			let json_body = r#"{}"#;
 
@@ -531,43 +640,6 @@ pub mod pallet {
 
 		}
 
-
-
-		/// A helper function to fetch the word and send signed transaction.
-		// pub fn fetch_word_and_send_signed(word: String) {
-		// 	let signer = Signer::<T, T::AuthorityId>::all_accounts();
-
-		// 	let results = signer.send_signed_transaction(|_account| {
-		// 		Call::save_bucket_name { name: word.clone() }
-		// 	});
-
-		// 	for (acc, res) in &results {
-		// 		match res {
-		// 			Ok(()) => log::info!("{:?} Word fetch success: {}.", acc.id, word),
-		// 			Err(e) =>
-		// 				log::error!("{:?}: submit transaction failure. Reason: {:?}", acc.id, e),
-		// 		}
-		// 	}
-		// }
-
-		pub fn fetch_folder_name_and_send_signed(word: String) {
-			let signer = Signer::<T, T::AuthorityId>::all_accounts();
-
-			let results = signer.send_signed_transaction(|_account| {
-				Call::save_folder_name { fname: word.clone() }
-			});
-
-			for (acc, res) in &results {
-				match res {
-					Ok(()) => log::info!("{:?} Word fetch success: {}.", acc.id, word),
-					Err(e) =>
-						log::error!("{:?}: submit transaction failure. Reason: {:?}", acc.id, e),
-				}
-			}
-		}
-
-		//get word from the user
-		
-
 	}
+
 }
